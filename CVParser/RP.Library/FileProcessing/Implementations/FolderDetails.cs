577b49ceb023a62d5b35c0b8d4d04c9679fd8bfb
Elio Cortés <elio.cortes3000@gmail.com>
2023-04-10 21:50:41 -0500

    Fix error references

--- CVParser/RP.Library/FileProcessing/Implementations/FolderDetails.cs
+++ CVParser/RP.Library/FileProcessing/Implementations/FolderDetails.cs
@@ -1,19 +1,18 @@
 ﻿using RP.Library.FileProcessing.Interfaces;
 
-namespace RP.Library.FileProcessing.Implementations
-{
-    internal class FolderDetails : IFolderDetails
-    {
-        private string _folder;
+namespace RP.Library.FileProcessing.Implementations;
 
-        public FolderDetails(string folder)
-        {
-            this._folder=folder;
-        }
+  internal class FolderDetails : IFolderDetails
+  {
+      private string _folder;
 
-        public bool ValidFolder()
-        {
-           return Directory.Exists(_folder);
-        }
-    }
-}
+      public FolderDetails(string folder)
+      {
+          this._folder=folder;
+      }
+
+      public bool ValidFolder()
+      {
+         return Directory.Exists(_folder);
+      }
+  }
---
+++ CVParser/RP.Library/FileProcessing/Interfaces/IInputReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RP.Library.FileProcessing.Interfaces;
+
+  public interface IInputReader
+  {
+  void SetNextReader(IInputReader value);
+  IList<string> ReadIntoList(string location);        
+  }
